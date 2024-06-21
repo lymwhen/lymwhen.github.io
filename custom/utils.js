29dc1d6901a4fdfb52ee91c0a13ed50dacb7eb3f
李彦明 <18469199738@189.cn>
2024-06-21 17:43:29 +0800

    解决 utils.js 报错

--- custom/utils.js
+++ custom/utils.js
@@ -1,10 +1,4 @@
 // DOM事件路径是否包含标签
 function pathContains(arr, tagName) {
-    tagName = tagName.toUpperCase();
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i].nodeName == tagName) {
-            return true;
-        }
-    }
-    return false;
+    return arr && arr.some(v => arr[i].nodeName == tagName.toUpperCase())
 }
